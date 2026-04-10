29f9b74382495d10cab40ec71ac583390ed1d7fd
tkcd <goriponikeike55@gmail.com>
2026-04-10 17:47:55 +0900

    feat: add network signal strength

--- plugins/network.sh
+++ plugins/network.sh
@@ -9,23 +9,39 @@ source "$current_dir/../lib/utils.sh"
 
 ethernet_icon=$(get_tmux_option "@tmux2k-network-ethernet-icon" "󰈀")
 wifi_icon=$(get_tmux_option "@tmux2k-network-wifi-icon" "")
+wifi_strength_0=$(get_tmux_option "@tmux2k-network-wifi-strength-0" "󰤯")
+wifi_strength_1=$(get_tmux_option "@tmux2k-network-wifi-strength-1" "󰤟")
+wifi_strength_2=$(get_tmux_option "@tmux2k-network-wifi-strength-2" "󰤢")
+wifi_strength_3=$(get_tmux_option "@tmux2k-network-wifi-strength-3" "󰤨")
 offline_icon=$(get_tmux_option "@tmux2k-network-offline-icon" "󰌙")
 
 get_ssid() {
     case $(uname -s) in
     Linux)
         if command -v nmcli > /dev/null 2>&1; then
-            SSID=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f 2)
+            SSID=$(nmcli -t -f active,ssid,signal dev wifi | grep '^yes' | cut -d: -f 2)
+            signal_perc=$(nmcli -t -f active,ssid,signal dev wifi | grep '^yes' | cut -d: -f 3)
+            if [ "$signal_perc" -gt 80 ]; then
+                icon="$wifi_strength_3"
+            elif [ "$signal_perc" -gt 60 ]; then
+                icon="$wifi_strength_2"
+            elif [ "$signal_perc" -gt 30 ]; then
+                icon="$wifi_strength_1"
+            else
+                icon="$wifi_strength_0"
+            fi
         elif command -v iwgetid >/dev/null 2>&1; then
             SSID=$(iwgetid -r)
+            icon="$wifi_icon"
         else
             wlaninfo=$(iw wlan0 link)
             if [ $? -eq 0 ]; then
                 SSID=$(awk -F ':' '/SSID/{print $2}' <<< "${wlaninfo}")
+                icon="$wifi_icon"
             fi
         fi
         if [ -n "$SSID" ]; then
-            printf '%s' "$wifi_icon $SSID"
+            printf '%s' "$icon $SSID"
         else
             echo "$ethernet_icon Eth"
         fi
